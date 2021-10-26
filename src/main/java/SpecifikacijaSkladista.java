881e7111e9c1abf769132d0a02dc95e4d7c8006b
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2021-10-26 15:26:50 +0200

    dodati korisnici i gson dependency

--- src/main/java/SpecifikacijaSkladista.java
+++ src/main/java/SpecifikacijaSkladista.java
@@ -1,4 +1,9 @@
+import java.util.List;
+
 public abstract class SpecifikacijaSkladista {
+
+
+    private List<Korisnik>korisnici;
 
     public abstract void createRoot(String path, String name);
     public abstract void checkIfRootExists(String path);
