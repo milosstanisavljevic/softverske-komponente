f1136938a24acd2c0fe1c12d5260a0913886299c
Miljana <mstanic5819rn@raf.rs>
2021-10-27 17:33:07 +0200

    Uradjene metode.

--- src/main/java/SpecifikacijaSkladista.java
+++ src/main/java/SpecifikacijaSkladista.java
@@ -1,6 +1,8 @@
 import com.google.gson.Gson;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -15,17 +17,14 @@ public abstract class SpecifikacijaSkladista {
     public abstract void checkIfRootExists(String path);
     public abstract void checkPrivileges();
 
-    public void makeConfig(String path) throws Exception {
+    public void makeConfig(String path, Object size, Object filetype, Object maxNumber, Object admin) throws Exception {
         try {
 
-            String ime = "proba1";
-            path = path + "\\" + ime + ".json";
-
             Map<String, Object> map = new HashMap<>();
-            map.put("size of root directory", 10000000);
-            map.put("file type", ".txt");
-            map.put("max number of files", 10);
-            map.put("admin", "Bica Bakic");
+            map.put("size of root directory", size);
+            map.put("file type", filetype);
+            map.put("max number of files", maxNumber);
+            map.put("admin", admin);
 
             Writer writer = new FileWriter(path);
             new Gson().toJson(map, writer);
@@ -38,13 +37,21 @@ public abstract class SpecifikacijaSkladista {
 
     }
 
-    public void makeUser(String path) throws Exception{
+    public void makeDefaultConfig(String path){ //POZIVATI OVO U IMPLEMENTACIJI
+
+        path = path + "\\" + "config.json";
+        try {
+            makeConfig(path, 1000000, ".txt", 10, "user1");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void makeUser(String path, String username, String password, boolean edit, boolean write, boolean read, boolean delete) throws Exception{
          try {
-             String ime = "proba2";
-             path = path + "\\" + ime + ".json";
 
-             Korisnik user = new Korisnik("Bica", "Bakic", true, true, true, true);
-
+             Korisnik user = new Korisnik(username, password, edit, write, read, delete);
+             //user.getUsername(), user.getPassword()
              Writer writer = new FileWriter(path);
              new Gson().toJson(user, writer);
 
@@ -55,9 +62,60 @@ public abstract class SpecifikacijaSkladista {
          }
     }
 
-    public void updateConfig() throws IOException {
-        // fajl koji sluzi za update config fajla
+    public void makeDefaultUser(String path){ // POZIVATI OVO U IMPLEMENTACIJI
+
+        path = path + "\\" + "users.json";
+
+        try {
+            makeUser(path, "user1", "user1", true, true, true, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
+    public void addUser(String path, String name, String password, int privilege) throws Exception{
+        //nije testirana
+        try {
+            if(privilege == 1){
+                path = path + "\\" + "users.json";
+                makeUser(path,name, password, false, false, true, false);
+
+            }
+            if(privilege == 2){
+                path = path + "\\" + "users.json";
+                makeUser(path, name, password, false, true, true, false);
+
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public void updateConfig(String path, Object size, Object filetype, Object maxNumber) throws Exception{
+        try {
+
+            String admin = "";
+
+            Gson gson = new Gson();
+            Reader reader = Files.newBufferedReader(Paths.get(path));
+            Map<String, Object> map = gson.fromJson(reader, Map.class);
+
+            for (Map.Entry<String, Object> entry: map.entrySet()) {
+                if(entry.getKey() == "admin"){
+                    admin = entry.getValue().toString();
+                }
+            }
+            reader.close();
+
+            //izbrisati sve iz fajla
+
+            makeConfig(path, size, filetype, maxNumber, admin);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
 
 
