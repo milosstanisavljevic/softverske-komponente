d37ca3eb091cb6539bb74ee4c795ddcf0e790ef2
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2021-10-30 13:57:08 +0200

    menadzer specifikacije

---
+++ src/main/java/Manager.java
@@ -0,0 +1,11 @@
+public class Manager {
+
+    private static SpecifikacijaSkladista specifikacijaSkladista;
+
+    public static void registerImpl(SpecifikacijaSkladista spec){
+        specifikacijaSkladista = spec;
+    }
+    public static SpecifikacijaSkladista getImpl(){
+        return specifikacijaSkladista;
+    }
+}
