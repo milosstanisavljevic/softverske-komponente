a234a5efdc5c5cce80f6c2fc5b1e8020b48c2d23
Miljana <mstanic5819rn@raf.rs>
2021-11-05 15:34:40 +0100

    update

--- src/main/java/SpecifikacijaSkladista.java
+++ src/main/java/SpecifikacijaSkladista.java
@@ -20,148 +20,38 @@ public abstract class SpecifikacijaSkladista {
     public abstract void createFolder(String folderName);
     public abstract void deleteFile(String name);
     public abstract void deleteFolder(String name);
-    public abstract void loadUsers();
 
-    public void makeConfig(String path, Object size, String filetype, int maxNumber, String admin) throws Exception {
-        try {
+    public Map<String, Object> mapConfig(Object size,String filetype, int maxNumber, String admin ){
+        Map<String, Object> map = new HashMap<>();
+        map.put("size of root directory", size);
+        map.put("file type", filetype);
+        map.put("max number of files", maxNumber);
+        map.put("admin", admin);
 
-            Map<String, Object> map = new HashMap<>();
-            map.put("size of root directory", size);
-            map.put("file type", filetype);
-            map.put("max number of files", maxNumber);
-            map.put("admin", admin);
-            //loc
-            Writer writer = new FileWriter(path);
-            new Gson().toJson(map, writer);
-
-            writer.close();
-
-        } catch (Exception e){
-            e.printStackTrace();
-        }
-
+        return map;
     }
 
-    public void makeDefaultConfig(String path, String username){
+    public abstract void makeConfig(String path, Map<String, Object> mapa);
 
-        path = path + "\\" + "config.json";
-        try {
-            makeConfig(path, 1000000, ".txt", 10, username);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public abstract void makeDefaultConfig(String path, String username);
+
+    public abstract void updateConfig(String path, int size, String filetype, int maxNumber);
+
+    public List<Korisnik> loadUsers(String username, String password, boolean edit, boolean write, boolean read, boolean delete){
+        Korisnik user = new Korisnik(username, password, edit, write, read, delete);
+        korisnici.add(user);
+        return korisnici;
     }
 
-    public void makeUser(String path, String username, String password, boolean edit, boolean write, boolean read, boolean delete) throws Exception{
-         try {
-             Korisnik user = new Korisnik(username, password, edit, write, read, delete);
-             korisnici.add(user);
-             //user.getUsername(), user.getPassword()
-             Writer writer = new FileWriter(path);
-             for (Korisnik k : korisnici) {
-                 new Gson().toJson(k, writer);
-             }
-             writer.close();
+    public abstract void makeUser(String path, List<Korisnik> korisnici);
 
-         }catch (Exception e){
-             e.printStackTrace();
-         }
-    }
+    public abstract void makeDefaultUser(String path, String username, String password);
 
-    public void makeDefaultUser(String path, String username, String password){
+    public abstract void addUser(String path, String name, String password, int privilege);
 
-        path = path + "\\" + "users.json";
+    public abstract String checkAdmin(String path);
 
-        try {
-            makeUser(path, username, password, true, true, true, true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void addUser(String path, String name, String password, int privilege) throws Exception{
-        try {
-            if(privilege == 1){
-                path = path + "\\" + "users.json";
-                makeUser(path,name, password, false, false, true, false);
-
-            }
-            if(privilege == 2){
-                path = path + "\\" + "users.json";
-                makeUser(path, name, password, false, true, true, false);
-
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-
-    public void updateConfig(String path, int size, String filetype, int maxNumber) throws Exception{
-        try {
-
-            path = path + "\\" + "config.json";
-            String admin = checkAdmin(path);
-            makeConfig(path, size, filetype, maxNumber, admin);
-            System.out.println(admin + " " + size + " " + filetype + " " + maxNumber + " " + path);
-
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
-
-    public String checkAdmin(String path){
-        try {
-            String admin = "";
-
-            Gson gson = new Gson();
-            Reader reader = Files.newBufferedReader(Paths.get(path));
-            Map<String, Object> map = gson.fromJson(reader, Map.class);
-
-            for (Map.Entry<String, Object> entry: map.entrySet()) {
-                if(entry.getKey().equalsIgnoreCase("admin")){
-                    admin += entry.getValue().toString();
-                }
-            }
-            reader.close();
-            return admin;
-        }catch (Exception e){
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    public boolean checkUser(String path, String username1, String password1){
-        try {
-            String username2 = "";
-            String password2 = "";
-            path = path + "\\" + "users.json";
-
-            Gson gson = new Gson();
-            Reader reader = Files.newBufferedReader(Paths.get(path));
-            Map<String, Object> map = gson.fromJson(reader, Map.class);
-
-            for (Map.Entry<String, Object> entry: map.entrySet()) {
-                if(entry.getKey().equalsIgnoreCase("username")){
-                    username2 += entry.getValue().toString();
-                }
-                if(entry.getKey().equalsIgnoreCase("password")){
-                    password2+= entry.getValue().toString();
-                }
-            }
-            reader.close();
-
-            if(password1.equalsIgnoreCase(password2) && username1.equalsIgnoreCase(username2)) {
-                return true;
-            }else{
-                return false;
-            }
-        }catch (Exception e){
-            e.printStackTrace();
-            return false;
-        }
-    }
+    public abstract boolean checkUser(String path, String username1, String password1);
 
 
 }
