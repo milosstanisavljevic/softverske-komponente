de70ac95260451b41e98b5ab53a2b41968f4b371
Miljana <mstanic5819rn@raf.rs>
2021-11-07 00:01:39 +0100

    update4

--- src/main/java/SpecifikacijaSkladista.java
+++ src/main/java/SpecifikacijaSkladista.java
@@ -22,6 +22,7 @@ public abstract class SpecifikacijaSkladista {
     public abstract void createFolder(String path, String folderName);
     public abstract void deleteFile(String path, String name);
     public abstract void deleteFolder(String path, String name);
+    public abstract void moveFromTo(String fromFolder, String toFolder, String file);
 
     public Map<String, Object> mapConfig(Object size,String filetype, int maxNumber, String admin ){
         Map<String, Object> map = new HashMap<>();
