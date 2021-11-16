fbac7f35a01f24ce01bc7cc2e1b08f68a0f69d61
milosstanisavljevic <mstanisavljevic8220r@raf.rs>
2021-11-17 00:25:59 +0100

    JAVADOC

--- src/main/java/Korisnik.java
+++ src/main/java/Korisnik.java
@@ -8,16 +8,34 @@ public class Korisnik {
     private boolean edit;
     private boolean delete;
 
-
+    /**
+     * Kreira korisnika skladista sa prosledjenim parametrima
+     * @param username - korisnicko ime
+     * @param password - lozinka za korisnicki nalog
+     */
 
     public Korisnik(String username, String password){
         this.username = username;
         this.password = password;
 
     }
+
+    /**
+     * Kreira objekat korisnika
+     */
     public Korisnik(){
 
     }
+
+    /**
+     * Kreira korisnika skladista sa prosledjenim privilegijama
+     * @param username - korisnicko ime
+     * @param password - lozinka
+     * @param edit - privilegija za menjanje fajlova u skladisty
+     * @param write - privilegija za pisanje fajlova u skladistu
+     * @param read - privilegija za citanje fajlova u skladistu
+     * @param delete - privilegija za brisanje fajlova iz skladista
+     */
     public Korisnik(String username, String password, boolean edit, boolean write, boolean read, boolean delete){
         this.username = username;
         this.password = password;
@@ -27,6 +45,12 @@ public class Korisnik {
         this.delete = delete;
 
     }
+
+    /**
+     * proverava da li je korisnik admin
+     * @return true ako je korisnik admin, ima sve privilegije
+     * @return false ukoliko nema sve privilegije
+     */
     public boolean checkAdmin(){
         if(read && write && delete && edit){
             return true;
